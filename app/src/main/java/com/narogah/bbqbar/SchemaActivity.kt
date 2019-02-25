9b8ce99385c267f61547cd2ce41a72130ca4c199
Ilya <ilya_chizhov@mail.ru>
2019-02-25 23:50:13 +0300

    Смена макета в обоих экранах

--- app/src/main/java/com/narogah/bbqbar/SchemaActivity.kt
+++ app/src/main/java/com/narogah/bbqbar/SchemaActivity.kt
@@ -25,47 +25,9 @@ class SchemaActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     override fun onClick(v: View?) {
-        when (v?.id) {
-            R.id.tb1 -> {
-                val i = Intent(this, BookingActivity::class.java)
-                startActivity(i)
-            }
-            R.id.tb2 -> {
-                val i = Intent(this, BookingActivity::class.java)
-                startActivity(i)
-            }
-            R.id.tb3 -> {
-                val i = Intent(this, BookingActivity::class.java)
-                startActivity(i)
-            }
-            R.id.tb4 -> {
-                val i = Intent(this, BookingActivity::class.java)
-                startActivity(i)
-            }
-            R.id.tb5 -> {
-                val i = Intent(this, BookingActivity::class.java)
-                startActivity(i)
-            }
-            R.id.tb6 -> {
-                val i = Intent(this, BookingActivity::class.java)
-                startActivity(i)
-            }
-            R.id.tb7 -> {
-                val i = Intent(this, BookingActivity::class.java)
-                startActivity(i)
-            }
-            R.id.tb8 -> {
-                val i = Intent(this, BookingActivity::class.java)
-                startActivity(i)
-            }
-            R.id.tb9 -> {
-                val i = Intent(this, BookingActivity::class.java)
-                startActivity(i)
-            }
-            R.id.tb10 -> {
-                val i = Intent(this, BookingActivity::class.java)
-                startActivity(i)
-            }
-        }
+        val id = v?.id
+        val i = Intent(this, BookingActivity::class.java)
+        i.putExtra("id", id)
+        startActivity(i)
     }
 }
