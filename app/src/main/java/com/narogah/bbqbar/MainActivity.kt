e049c3e6d6793891b85072c96417703d1986225d
Артём <radivilovartem@gmail.com>
2019-02-27 00:22:31 +0300

    Проверка на наличие интеренета + иконка

--- app/src/main/java/com/narogah/bbqbar/MainActivity.kt
+++ app/src/main/java/com/narogah/bbqbar/MainActivity.kt
@@ -1,20 +1,27 @@
 package com.narogah.bbqbar
 
+import android.content.Context
 import android.content.Intent
+import android.net.ConnectivityManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import com.narogah.bbqbar.R.id.btn
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        btn.setOnClickListener {
-            val i = Intent(this, SchemaActivity::class.java)
-            startActivity(i)
+        val connMgr = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val networkInfo = connMgr.activeNetworkInfo
+        if (networkInfo != null && networkInfo.isConnected) {
+            setContentView(R.layout.activity_main)
+            btn.setOnClickListener {
+                val i = Intent(this, SchemaActivity::class.java)
+                startActivity(i)
+            }
+        } else {
+            setContentView(R.layout.activity_main_no_internet)
         }
     }
 }
